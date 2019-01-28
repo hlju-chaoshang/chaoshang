4391dd7d9e7c33f2a451668d226500ee19c5709a
moots <moots@ali.com>
2019-01-28 16:49:21 +0800

    Initial commit

---
+++ src/main/java/hlju/chaoshang/ChaoshangApplication.java
@@ -0,0 +1,14 @@
+package hlju.chaoshang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaoshangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaoshangApplication.class, args);
+    }
+
+}
+
