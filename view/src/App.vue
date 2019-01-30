7a8d015edf00091b74d2098afc944de00d25ba9b
“txoyp <1401741458@qq.com>
2019-01-30 15:00:22 +0800

    1_30

--- view/src/App.vue
+++ view/src/App.vue
@@ -3,27 +3,28 @@
     <router-view/>
     <div id="nav">
       <router-link to="/">登录</router-link> |
-      <router-link to="/register">注册</router-link>
+      <router-link to="/register">注册</router-link> |
+      <router-link to="/signUp">报名</router-link>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
